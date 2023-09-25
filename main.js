e040d4bdd6deb71eff3385a5e88740a0f393f55d
Pratyush Tiwary <36852896+pratyushtiwary@users.noreply.github.com>
2023-09-26 02:22:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const createWindow = () => {
     callback({
       responseHeaders: {
         ...details.responseHeaders,
-        'Content-Security-Policy': ["default-src 'self' data:"]
+        'Content-Security-Policy': ["default-src 'self' 'unsafe-eval' data:"]
       }
     })
   })
