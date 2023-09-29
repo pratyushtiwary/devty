d6fe0195823c81abc495b8bd3c8331c0d6b9244e
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2023-09-29 14:20:46 +0530

    module list generator is now more verbose and stable

--- moduleListGenerator.ts
+++ moduleListGenerator.ts
@@ -3,6 +3,63 @@ import fs from 'fs'
 import path from 'path'
 import { type Routes } from './src/types/route'
 
+const CONFIG_REQUIRED_KEYS = ['slug', 'name', 'icon|image']
+
+/*
+  Given a path to a module this function returns whether a module is valid or not,
+  along with the reason
+*/
+function isValidModule(loc: string): [boolean, string | null] {
+  /*
+    A module is valid if:
+    - config.json exists and is a valid json file,
+    - config.json contains CONFIG_REQUIRED_KEYS,
+    - a .vue file exists with the same name as the dir,
+  */
+  if (!fs.existsSync(loc)) {
+    return [false, "Provided location doesn't exists"]
+  }
+  const CONFIG_PATH = path.join(loc, 'config.json'),
+    VUE_PATH = path.join(loc, loc.split(path.sep).pop() + '.vue')
+  let config = null
+
+  if (!fs.existsSync(CONFIG_PATH)) {
+    return [false, 'Failed to find config.json file']
+  }
+
+  try {
+    config = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'))
+  } catch (_) {
+    return [false, 'Failed to parse config.json, is it a valid json file?']
+  }
+
+  const CONFIG_KEYS = Object.keys(config)
+
+  for (let i = 0; i < CONFIG_REQUIRED_KEYS.length; i++) {
+    const keys = CONFIG_REQUIRED_KEYS[i].split(/\|/g)
+    let containsSingleKey = false
+    for (let j = 0; j < keys.length; j++) {
+      const key = keys[j]
+      if (Boolean(key) && CONFIG_KEYS.includes(key)) {
+        containsSingleKey = true
+        break
+      }
+    }
+    if (!containsSingleKey) {
+      return [
+        false,
+        `config.json doesn't contains ${CONFIG_REQUIRED_KEYS[i].replace('|', ' or ')} key`
+      ]
+    }
+  }
+
+  if (!fs.existsSync(VUE_PATH)) {
+    return [false, `Failed to find ${loc.split(path.sep).pop()}.vue file inside the module`]
+  }
+
+  return [true, null]
+}
+
 function generateRoutes() {
   console.clear()
   console.log('Generating Routes List from Modules...')
@@ -28,13 +85,10 @@ function generateRoutes() {
     const modulePath = path.join(modulesPath, modules[i]),
       moduleConfigPath = path.join(modulePath, 'config.json')
 
-    if (!fs.existsSync(moduleConfigPath)) {
-      console.warn(`Skipping ${modules[i]} module, no config file found`)
-      continue
-    }
+    const [moduleValid, reason] = isValidModule(modulePath)
 
-    if (!fs.existsSync(path.join(modulePath, modules[i] + '.vue'))) {
-      console.warn(`Skipping ${modules[i]} module, no ${modules[i]}.vue file found`)
+    if (!moduleValid) {
+      console.log(`Skipping module ${modules[i]}, reason: ${reason}`)
       continue
     }
 
